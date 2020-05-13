3cfa683cbb523a74e53f9788f4c0ee740681691c
wangzhiwei11 <1372101837@qq.com>
2020-05-13 23:02:16 +0800

    增加Eureka服务注册中心

---
+++ cloud-eureka-server7001/src/main/java/com/wangzw/springcloud/EurekaMain7001.java
@@ -0,0 +1,17 @@
+package com.wangzw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: wangzw
+ * @date: 2020-05-13 22:37
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class,args);
+    }
+}
