52464e93639628021ce59790f408a01a497c2e0a
wangzhiwei11 <1372101837@qq.com>
2020-07-29 18:04:22 +0800

    gateway

---
+++ cloud-gateway9527/src/main/java/com/wangzw/springcloud/GateWayMain9527.java
@@ -0,0 +1,17 @@
+package com.wangzw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author wangzw
+ * @date 2020/7/29 17:33
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class,args);
+    }
+}
