d28506eb94424ecdf717bd8b8d66364242a062c0
wangzhiwei11 <1372101837@qq.com>
2020-07-30 18:18:49 +0800

    config

---
+++ cloud-config-center3344/src/main/java/com/wangzw/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,17 @@
+package com.wangzw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author wangzw
+ * @date 2020/7/30 16:21
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
