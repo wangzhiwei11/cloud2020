b1a4b55373450a9d62a45379366a47f39918d6cd
wangzhiwei11 <1372101837@qq.com>
2020-07-31 17:37:36 +0800

    config-bus配置通知

---
+++ cloud-config-client3366/src/main/java/com/wangzw/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,17 @@
+package com.wangzw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author wangzw
+ * @date 2020/7/31 16:40
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
